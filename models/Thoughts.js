e8a749c40630e58a7005829ae61f1517e09d38cb
Girish Madnani <girishmadnani05@gmail.com>
2022-05-29 19:22:34 +0100

    thought model

---
+++ models/Thoughts.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./reactionSchema");
+// Import dayjs for date-time formatting
+const dayjs = require("dayjs");
+
+// Thought model schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: { type: Date, default: Date.now, get: (createdAt) => dayjs(createdAt).format("DD-MM-YY HH:mm") },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  {
+    // Virtuals enabled for reactions count
+    toJSON: { virtuals: true, getters: true },
+  }
+);
+
+// Virtual for reaction count
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Thought model init
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
