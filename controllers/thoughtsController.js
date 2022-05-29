288aa03d12e90b40f8050c1e98adcfce0d7a53f0
Girish Madnani <girishmadnani05@gmail.com>
2022-05-29 19:06:25 +0100

    thought routes

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,104 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  
+  // Get single thought
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+      !thought ? res.status(404).json({ message: "No thought with that ID" }) : res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  // Get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  // Create thought
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      const user = await User.findOneAndUpdate(
+        { _id: req.body.userId }, { $addToSet: { thoughts: thought._id.toHexString() } }, { new: true }
+      );
+      console.log(`Thought created by user: ${user._id}`);
+      return res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  // Update thought
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true }
+      );
+      // Check if thought exists
+      !thought
+        ? res
+            .status(404)
+            .json({ message: "user doesn't exist!" })
+        : res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  // Remove thought
+  async removeThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndRemove({
+        _id: req.params.thoughtId,
+      });
+      // Check if thought exists
+      if (!thought) {
+        res
+          .status(404)
+          .json({ message: "user doesn't exist!" });
+      } else {
+        res.json({ message: "Thought removed" });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Create reaction
+  async createReaction(req, res) {
+    try {
+      const thought = await Thought.findByIdAndUpdate(
+        { _id: req.params.thoughtId }, { $addToSet: { reactions: req.body } }, { runValidators: true, new: true }
+      );
+      !thought
+        ? res
+            .status(404)
+            .json({ message: "user doesn't exist!" })
+        : res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Remove reaction
+  async removeReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId }, { $pull: { reactions: { reactionId: req.body.reactionId } } }, { new: true }
+      );
+      !thought
+        ? res.status(404).json({ message: "thought doesn't exist!" })
+        : res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+const { getSingleThought, getThoughts, createThought, updateThought, removeThought, createReaction, removeReaction } = require("../../controllers/thoughtsController");
+
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(removeThought);
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(createReaction).delete(removeReaction);
+
+module.exports = router;
